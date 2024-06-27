e8b792c866c28dd6515301080af43495f5d3412f
gabriel-leal <gabrielealmenezes@gmail.com>
2024-06-27 16:04:55 -0300

    termos e direitos

--- scripts/inscricao.js
+++ scripts/inscricao.js
@@ -1,3 +1,15 @@
+const modal = document.querySelector("dialog")
+
+function showmodal() {
+    modal.showModal()
+}
+
+function hidemodal() {
+    modal.close()
+}
+
+
+
 // máscaras de formulário
 $('#idata').mask('00/00/0000');
 $('#itel').mask('(00) 00000-0000');
