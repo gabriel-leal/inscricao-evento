ab42bac1c16ca3b6d8c4f065bc9ba1544d439a4f
gabriel-leal <gabrielealmenezes@gmail.com>
2024-06-28 09:57:51 -0300

    Update inscricao.js

--- scripts/inscricao.js
+++ scripts/inscricao.js
@@ -1,15 +1,19 @@
 const modal = document.querySelector("dialog")
+let nome = document.getElementById('inome')
+let datanas = document.getElementById('idata')
+let tel = document.getElementById('itel')
+let mem =  document.getElementById('imem')
+let conv = document.getElementById('iconv')
+let direitos = document.getElementById('idireitos')
 
 function showmodal() {
     modal.showModal()
-    modal.scrollTop = 1;
+    modal.scrollTop()
 }
 
 function hidemodal() {
     modal.close()
 }
-
-
 
 // máscaras de formulário
 $('#idata').mask('00/00/0000');
